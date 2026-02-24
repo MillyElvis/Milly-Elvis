d061361c2317d8da1185a855a62a6632aa68a693
MillyElvis <MillyElvis@users.noreply.github.com>
2026-02-24 01:29:38 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.ts
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
   build: {
-    outDir: 'dist',
-  }
-});
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        baybridge: resolve(__dirname, 'baybridge.html'), // 👈 add this
+      },
+    },
+  },
+})
